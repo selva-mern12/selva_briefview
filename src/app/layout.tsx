91174ad9a7716ea766a6a578d45d93b42d8e1ec5
selva-mern12 <selva.mern12@gmail.com>
2025-06-18 15:08:41 +0530

    google tag added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -109,6 +109,15 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <Script id="gtm-head" strategy="beforeInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id=GTM-W46LL7GR'+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-W46LL7GR');
+          `}
+        </Script>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
@@ -167,6 +176,14 @@ export default function RootLayout({
             });
           `}
         </Script>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-W46LL7GR"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          ></iframe>
+        </noscript>
         <noscript>You need to enable JavaScript to run this app.</noscript>
         {children}
       </body>
