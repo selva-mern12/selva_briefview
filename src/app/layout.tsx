423d8b5e69449ab95cfe879f51020c8180cc9344
selva-mern12 <selva.mern12@gmail.com>
2025-06-24 14:24:45 +0530

    Resume Update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from 'next';
-import Script from 'next/script';
 import './globals.css';
 
 export const metadata: Metadata = {
@@ -109,15 +108,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <Script id="gtm-head" strategy="beforeInteractive">
-          {`
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id=GTM-W46LL7GR'+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-W46LL7GR');
-          `}
-        </Script>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
@@ -160,22 +150,6 @@ export default function RootLayout({
         />
       </head>
       <body>
-        {/* Google Analytics */}
-        <Script
-          src="https://www.googletagmanager.com/gtag/js?id=G-1KBZDHEM2Q"
-          strategy="beforeInteractive"
-        />
-        <Script id="ga-init" strategy="beforeInteractive">
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-1KBZDHEM2Q', {
-              anonymize_ip: true,
-              cookie_flags: 'SameSite=None;Secure',
-            });
-          `}
-        </Script>
         <noscript>
           <iframe
             src="https://www.googletagmanager.com/ns.html?id=GTM-W46LL7GR"
